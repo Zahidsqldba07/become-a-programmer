f97c9bd5271ad43162402b91879606afdb72e38a
August Frisk <35209857+parasiticfrisk@users.noreply.github.com>
2022-07-08 05:42:18 -0700

    real world examples
    All assignments, notes, and files related to the LinkedIn learning course Programming Foundations: Real-World Examples.

---
+++ real_examples/excercise_files/05lists_tuples/05_02/start_05_02_parking_lots.py
@@ -0,0 +1,17 @@
+""" A 3-Dimensional Valet Service """
+
+# 2D list of lists
+#  - index cars by row, spot
+lot_2d = [
+    ["Toyota", "Audi", "BMW"],  # 0th row
+    ["Lexus", "Jeep"],  # 1st row
+    ["Honda", "Kia", "Mazda"],
+]  # 2nd row
+
+# 3D list of lists of lists
+#  - index cars by floor, row, spot
+lot_3d = [
+    [["Telsa", "Fiat", "BMW"], ["Honda", "Jeep"], ["Saab", "Kia", "Ford"]],  # 0th floor
+    [["Subaru", "Nissan"], ["Volvo"]],  # 1st floor
+    [["Mazda", "Chevy"], [], ["Volkswagen"]],  # 2nd floor
+]
